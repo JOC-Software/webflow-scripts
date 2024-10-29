d28136821e8ce67dd57f9c5e1500540a3f33773a
William Bellity <william.bellity1@gmail.com>
2024-10-29 09:31:53 +0000

    :ambulance: (shop) handle updatePreview when some button are clicked

--- src/generateBusinessCardPreviewUrl.js
+++ src/generateBusinessCardPreviewUrl.js
@@ -6,6 +6,8 @@ const CLOUDINARY_BASE_URL =
 const FONT = "Roboto";
 const FONT_SIZE = "64";
 const LOGO_SIZE = "h_400,w_600,c_limit";
+const LOGO_URL_PLACEHOLDER =
+  "https://cdn.prod.website-files.com/6553517a27f02d8ced998612/66fbd508864665c646883b4b_transparent-placeholder.png";
 
 const CARD_VARIANT_PARAMS = {
   49960146305355: {
@@ -62,30 +64,21 @@ const CARD_VARIANT_PARAMS = {
 
 // UTILS
 function base64UrlEncode(str) {
-  // Convert string to Base64
   let base64 = btoa(str);
-
-  // Replace Base64 characters to make it URL safe
   const base64UrlEncoded = base64
-    .replace(/\+/g, "-") // Replace + with -
-    .replace(/\//g, "_") // Replace / with _
-    .replace(/=+$/, ""); // Remove any = padding
-
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
   return base64UrlEncoded;
 }
-
 // END UTILS
 
 function generateBusinessCardPreviewUrl(name, logoUrl) {
-  // Get the variant from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const variant = urlParams.get("variant");
-
-  // Get the right params from card_variant_params
   const variantParams = CARD_VARIANT_PARAMS[variant] || {};
 
   let textOption = "";
-
   if (name) {
     textOption = `l_text:${FONT}_${FONT_SIZE}:${name},co_rgb:${variantParams.textColor},g_south_east,x_89,y_71/`;
   }
@@ -93,53 +86,67 @@ function generateBusinessCardPreviewUrl(name, logoUrl) {
   let logoOption = "";
   let path = variantParams.path;
 
-  if (logoUrl) {
+  if (logoUrl !== LOGO_URL_PLACEHOLDER) {
     const base64logoUrl = base64UrlEncode(logoUrl);
     logoOption = `l_fetch:${base64logoUrl},g_center,${LOGO_SIZE}/`;
   } else {
-    // do not show the engraved part if there is no logo
     path = path.split("-")[0];
   }
 
   return `${CLOUDINARY_BASE_URL}${textOption}${logoOption}${path}`;
 }
 
-// Wait for the DOM to fully load
+function updatePreview() {
+  const productImagesDiv = document.getElementById("product-images");
+  const customName = document.getElementById("custom-name").value;
+  const logoUrl = document.getElementById("logo-display")?.src;
+
+  if (!customName && logoUrl == LOGO_URL_PLACEHOLDER) {
+    return;
+  }
+
+  const encodedCustomName = encodeURIComponent(customName);
+  const imagePreview = generateBusinessCardPreviewUrl(
+    encodedCustomName,
+    logoUrl
+  );
+
+  const img = document.createElement("img");
+  img.src = imagePreview;
+  img.alt = "Customized Business Card Preview";
+
+  if (productImagesDiv) {
+    productImagesDiv.innerHTML = "";
+    productImagesDiv.appendChild(img);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Select the button with id 'show-image-button'
-  var button = document.getElementById("show-image-button");
+  const button = document.getElementById("show-image-button");
+  button.addEventListener("click", updatePreview);
 
-  // Add a click event listener to the button
-  button.addEventListener("click", function () {
-    // Select the div with id 'product-images'
-    const productImagesDiv = document.getElementById("product-images");
-
-    // Get the value from the input field with id 'custom-name'
-    const customName = document.getElementById("custom-name").value;
-
-    // URL encode the custom name to make it safe for use in the URL
-    const encodedCustomName = encodeURIComponent(customName);
-
-    // Create a new img element
-    var img = document.createElement("img");
-
-    // Create cloudinary preview
-    const logoUrl = document.getElementById("logo-display")?.src;
-    const imagePreview = generateBusinessCardPreviewUrl(
-      encodedCustomName,
-      logoUrl
-    );
-
-    // Set the src of the image, replacing 'TextOnCard' with the encoded custom name
-    img.src = imagePreview;
-
-    // Optionally, set the image's alt text
-    img.alt = "Customized Business Card Preview";
-
-    // Replace the div content with the image
-    if (productImagesDiv) {
-      productImagesDiv.innerHTML = ""; // Clear any existing content
-      productImagesDiv.appendChild(img); // Append the image
-    }
+  const variantOptions = document.querySelectorAll(".custom-option-selector");
+  variantOptions.forEach((option) => {
+    option.addEventListener("click", function () {
+      setTimeout(updatePreview, 100);
+    });
   });
+
+  const logoDisplay = document.getElementById("logo-display");
+  if (logoDisplay) {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (
+          mutation.type === "attributes" &&
+          mutation.attributeName === "src"
+        ) {
+          updatePreview();
+        }
+      });
+    });
+    observer.observe(logoDisplay, {
+      attributes: true,
+      attributeFilter: ["src"],
+    });
+  }
 });
